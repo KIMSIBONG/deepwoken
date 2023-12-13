ee6d8f2b5b54f184528a1939d641fe7207e87a2e
KIMSIBONG <153712936+KIMSIBONG@users.noreply.github.com>
2023-12-14 00:22:04 +0900

    여러가지 수정2

--- Assets/PlayerAnimation.cs
+++ Assets/PlayerAnimation.cs
@@ -1,9 +1,10 @@
+using UnityEditor.Animations;
 using UnityEngine;
 
 public class PlayerAnimation : MonoBehaviour
 {
     private Animator animator;
-
+    public float AttackNumber = 0f;
     void Start()
     {
         // Animator 컴포넌트 가져오기
@@ -12,11 +13,29 @@ public class PlayerAnimation : MonoBehaviour
 
     void Update()
     {
+
         // 예를 들어, 스페이스 바를 누르면 "Attack" 애니메이션을 재생
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetMouseButtonDown(0))
         {
             // "Attack" 애니메이션을 재생
-            animator.SetTrigger("AttackTrigger");
+            animator.SetTrigger("Attack");
+
+            // AttackNumber를 증가시키고, 범위를 0부터 2까지로 유지
+            AttackNumber++;
+            if (AttackNumber > 2)
+            {
+                AttackNumber = 0;
+            }
+
+            // "Blend" 파라미터 설정
+            animator.SetFloat("Blend", AttackNumber);
         }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            animator.SetTrigger("Parrying");
+            
+        }
+        
     }
+    
 }
