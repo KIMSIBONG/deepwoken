f8b96bd03dde0e538c19f41410b6d4c738023dae
KIMSIBONG <153712936+KIMSIBONG@users.noreply.github.com>
2023-12-16 13:53:00 +0900

    여러가지수정 및 애니메이션 수정

--- Assets/PlayerAnimation.cs
+++ Assets/PlayerAnimation.cs
@@ -5,8 +5,9 @@ public class PlayerAnimation : MonoBehaviour
 {
     private Animator animator;
     private float lastClickTime; // 마지막 클릭 시간 기록
+    private float lastParryTime;
     public float clickCooldown = 3f; // 클릭 쿨타임 설정 (초)
-
+    public float parryCooldown = 5f;
     void Start()
     {
         // Animator 컴포넌트 가져오기
@@ -22,8 +23,8 @@ public class PlayerAnimation : MonoBehaviour
             if (Input.GetMouseButtonDown(0))
             {
                 // 현재 실행 중인 모든 애니메이션을 멈춤
-                animator.speed = 0f;
-                
+                StopAllAnimations();
+
                 // "Attack" 애니메이션을 재생
                 animator.SetTrigger("Attack");
 
@@ -35,60 +36,74 @@ public class PlayerAnimation : MonoBehaviour
                 lastClickTime = Time.time;
 
                 // 애니메이션을 재생하고 나서 다시 정상 속도로 변경
-                animator.speed = 1f;
+                
             }
+
         }
+        if (Time.time - lastParryTime >= parryCooldown)
+        {   
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                StopAllAnimations();
 
-        if (Input.GetKeyDown(KeyCode.F))
-        {
+                animator.SetTrigger("Parrying");
+
+                lastParryTime = Time.time;
+            }
             // 현재 실행 중인 모든 애니메이션을 멈춤
-            animator.speed = 0f;
-
-            animator.SetTrigger("Parrying");
-
+            
             // 애니메이션을 재생하고 나서 다시 정상 속도로 변경
-            animator.speed = 1f;
-        }
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            // 현재 실행 중인 모든 애니메이션을 멈춤
-            animator.speed = 0f;
 
-            animator.SetTrigger("Run");
-
-            // 애니메이션을 재생하고 나서 다시 정상 속도로 변경
-            animator.speed = 1f;
-        }
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            // 현재 실행 중인 모든 애니메이션을 멈춤
-            animator.speed = 0f;
-
-            animator.SetTrigger("Sideright");
-
-            // 애니메이션을 재생하고 나서 다시 정상 속도로 변경
-            animator.speed = 1f;
-        }
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            // 현재 실행 중인 모든 애니메이션을 멈춤
-            animator.speed = 0f;
-
-            animator.SetTrigger("Walk");
-
-            // 애니메이션을 재생하고 나서 다시 정상 속도로 변경
-            animator.speed = 1f;
         }
         if (Input.GetKeyDown(KeyCode.D))
         {
             // 현재 실행 중인 모든 애니메이션을 멈춤
-            animator.speed = 0f;
 
-            animator.SetTrigger("Sideright");
 
-            // 애니메이션을 재생하고 나서 다시 정상 속도로 변경
-            animator.speed = 1f;
+            animator.SetTrigger("SideStep");
+        }
+        
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            // 현재 실행 중인 모든 애니메이션을 멈춤
+            animator.SetTrigger("Walk");
+
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            // 현재 실행 중인 모든 애니메이션을 멈춤
+
+
+            animator.SetTrigger("Run");
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            // 현재 실행 중인 모든 애니메이션을 멈춤
+
+
+            animator.SetTrigger("SideStep");
         }
 
+        if (!Input.anyKey)
+        {
+            // 아무 키나 안눌리면 "Idle" 애니메이션을 재생
+            StopAllAnimations();
+            animator.SetTrigger("Idle");
+            
+        }
+
+        // 아래는 다른 입력에 대한 처리 코드들...
+    }
+
+    // 현재 실행 중인 모든 애니메이션을 멈추는 메서드
+    private void StopAllAnimations()
+    {
+        
+        
+        
+        animator.ResetTrigger("Run");
+        animator.ResetTrigger("SideStep");
+        animator.ResetTrigger("Walk");
+        // 다른 애니메이션 트리거도 필요에 따라 추가
     }
 }
