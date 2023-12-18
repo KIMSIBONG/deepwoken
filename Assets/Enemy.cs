60c9dc0e9dc62bc958c541e7dd61f2e59320e93c
KIMSIBONG <153712936+KIMSIBONG@users.noreply.github.com>
2023-12-18 18:32:35 +0900

    애니메이션 생성

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    
     public Transform player;
     public float speed;
-
+    public Vector3 home;
     public float atkCooltime = 4;
     public float atkDelay;
+
     // Start is called before the first frame update
     void Start()
     {
+
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        home = transform.position;
+
     }
 
     // Update is called once per frame
@@ -20,6 +25,7 @@ public class Enemy : MonoBehaviour
     {
         if(atkDelay>=0)
             atkDelay -= Time.deltaTime;
+        
     }
     
     
--- Assets/Idlestate.cs
+++ Assets/Idlestate.cs
@@ -5,21 +5,24 @@ using UnityEngine;
 public class Idlestate : StateMachineBehaviour
 {
     Transform enemyTransform;
+    Transform player;
     Enemy enemy;
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         enemy = animator.GetComponent<Enemy>();
         enemyTransform = animator.GetComponent<Transform>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     }
 
     
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        if (Vector3.Distance(enemyTransform.position, enemy.player.position) <= 4)
-            animator.SetBool("MonsterRun", true);
+        if (Vector3.Distance(enemyTransform.position, enemy.player.position) <= 7)
+            animator.SetBool("IsFollow", true);
+            enemyTransform.LookAt(player);
 
         if (enemy.atkDelay <= 0)
-        animator.SetTrigger("MonsterAttack");
+        animator.SetTrigger("AttackState");
         
     }
 
