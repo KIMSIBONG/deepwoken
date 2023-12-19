d3816f5f5f39bf62f3115fbc8666361b9884dc82
KIMSIBONG <153712936+KIMSIBONG@users.noreply.github.com>
2023-12-19 17:37:19 +0900

    수정

--- Assets/PlayerAttack.cs
+++ Assets/PlayerAttack.cs
@@ -2,63 +2,50 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-    public float knockbackForce = 10f; // 넉백 힘
-    public GameObject knockbackObject; // 넉백을 받을 오브젝트
-    public GameObject particlePrefab; // 파티클 프리팹
-    private float lastClickTime = 0f;
-    public float mouseCooldown = 1f;
+    public float clickCooldown = 1f;  // 클릭 쿨다운 시간
+    public float sphereRadius = 0.5f; // 구의 반지름
+    private float lastClickTime;       // 마지막 클릭 시간
 
+    public GameObject particlePrefab;  // 충돌 시 소환될 파티클 프리팹
+    public float knockbackForce = 2f;  // KnockBack에 가해질 힘
 
-    void Update()
+    private void Update()
     {
-        float currentTime = Time.time;
         // 마우스 왼쪽 버튼 클릭 감지
-        if (Input.GetMouseButtonDown(0) && (currentTime - lastClickTime) >= mouseCooldown)
+        if (Input.GetMouseButtonDown(0) && Time.time - lastClickTime > clickCooldown)
         {
-            // attack 함수 실행
+            lastClickTime = Time.time;
             Attack();
-            lastClickTime = currentTime;
         }
     }
 
-    void Attack()
+    private void Attack()
     {
-        // 현재 오브젝트의 위치를 기준으로 'parrying' 태그를 가진 오브젝트 검사
-        float radius = 0.5f; // 구의 반지름 (크기의 반값)
-        Debug.Log("attack");
-        // Physics.OverlapSphere을 사용하여 구 형태의 충돌체 검출
-        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+        // 구 형태의 콜라이더 충돌 여부 체크
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, sphereRadius);
 
-        foreach (var collider in colliders)
+        foreach (Collider collider in hitColliders)
         {
-            // 검사한 오브젝트가 'parrying' 태그를 가진 경우
+            // parrying 태그를 가진 오브젝트와 충돌 시
             if (collider.CompareTag("parrying"))
             {
-                // 'parrying' 태그를 가진 오브젝트와 충돌했을 때 실행할 코드
-                Knockback(collider.gameObject);
+                Instantiate(particlePrefab, collider.transform.position, Quaternion.identity);
+                KnockBack(collider.gameObject);
+            }
 
-                // 부딪힌 위치에 프리팹 소환
-                ParryEffect(collider.ClosestPoint(transform.position));
+            // Enemy 태그를 가진 오브젝트와 충돌 시
+            if (collider.CompareTag("Enemy"))
+            {
+                Debug.Log("Damage");
             }
         }
     }
 
-    void Knockback(GameObject parryingObject)
+    private void KnockBack(GameObject obj)
     {
-        Debug.Log("parry");
-        // parryingObject에도 넉백 힘을 가함 (x와 z 방향으로만)
-        Rigidbody knockbackRigidbody = knockbackObject.GetComponent<Rigidbody>();
-        if (knockbackRigidbody != null)
-        {
-            Vector3 knockbackDirection = (knockbackObject.transform.position - transform.position).normalized;
-            knockbackDirection.y = 0; // y 방향은 0으로 설정하여 x와 z 방향만으로 넉백 발생
-            knockbackRigidbody.AddForce(knockbackDirection * knockbackForce, ForceMode.Impulse);
-        }
-    }
-    void ParryEffect(Vector3 position)
-    {
-        // 프리팹을 복제해서 소환
-        GameObject spawnedPrefab = Instantiate(particlePrefab, position, Quaternion.identity);
-        // 여기서는 Quaternion.identity를 사용하여 회전을 적용하지 않음
+        // KnockBack 오브젝트가 뒤로 밀리게 설정
+        Vector3 knockbackDirection = transform.position - obj.transform.position;
+        knockbackDirection.y = 0;
+        obj.GetComponent<Rigidbody>().AddForce(knockbackDirection.normalized * knockbackForce, ForceMode.Impulse);
     }
 }
