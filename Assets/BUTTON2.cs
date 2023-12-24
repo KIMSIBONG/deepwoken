cd5a6196ab11db758349de957f7272206ec966fd
KIMSIBONG <153712936+KIMSIBONG@users.noreply.github.com>
2023-12-25 02:41:18 +0900

    수정

--- Assets/BUTTON2.cs
+++ Assets/BUTTON2.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class BUTTON : MonoBehaviour
+public class BUTTON2 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -16,9 +16,8 @@ public class BUTTON : MonoBehaviour
     {
         
     }
-    public void ClickStart()
+    public void ClickEnd()
     {
-        SceneManager.LoadScene("Stage1");
+        SceneManager.LoadScene("Start");
     }
-    
 }
