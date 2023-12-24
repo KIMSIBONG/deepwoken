ee236385e6cbf30fee3e27abc0c2b298c8adf240
KIMSIBONG <153712936+KIMSIBONG@users.noreply.github.com>
2023-12-25 02:33:10 +0900

    2

---
+++ Assets/Portal.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        // 충돌한 오브젝트가 Player 태그를 가지고 있는지 확인
+        if (other.CompareTag("Player"))
+        {
+            // 다음 씬으로 이동
+            SceneManager.LoadScene("Stage2");
+        }
+    }
+}
